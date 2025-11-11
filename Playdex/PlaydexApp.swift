67b7c28a963fdadd3cfa6f3c0877fdce37f36de3
Bastien Soucasse <bastien.soucasse@icloud.com>
2025-11-11 17:23:14 +0100

    Initial Commit

---
+++ Playdex/PlaydexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaydexApp.swift
+//  Playdex
+//
+//  Created by Bastien Soucasse on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PlaydexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
